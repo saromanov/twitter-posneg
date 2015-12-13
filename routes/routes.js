a9cf3eec84a367cf88954034a736b507010fb9e2
Sergey Romanov <xxsmotur@gmail.com>
2015-12-13 19:49:15 +0500

    Routers for future features

--- routes/routes.js
+++ routes/routes.js
@@ -1,31 +1,15 @@
+var app = app || {};
+
 var mongoose = require('mongoose');
-var path = require('path');
-var Twitter = require('twitter');
-var sentiment = require('sentiment');
 
 mongoose.connect('mongodb://localhost/test');
 
-var Tweet = mongoose.model('Tweet', {author: String, tweet: String});
+var Model = mongoose.model('Model', {hashtag: String, posvalue: Number, negvalue: Number});
+app.Model = Model;
 
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
-});
-
-
-// Show current stream
-exports.getStream = function(req, res){
-    var count = 0;
-    client.stream('statuses/filter', {track: 'bad'}, function(stream) {
-        res.send('Monitoring');
-        stream.on('data', function(tweet) {
-            if(tweet.lang === "en") {
-                sentiment(tweet.text, function(err, result){
-                    console.log(result.score);
-                });
-            }
-        });
+// Show past results from values
+exports.getData = function(req, res){
+    Model.find(function(arr, data){
+        res.send(data);
     });
 };
