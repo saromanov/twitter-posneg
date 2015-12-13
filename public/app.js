7d074bb0e033dd53de561babfe45e478a8f62e66
Sergey Romanov <xxsmotur@gmail.com>
2015-12-13 18:42:53 +0500

    Remove comments

--- public/app.js
+++ public/app.js
@@ -1,22 +1,7 @@
 var app = app || {};
 
 $(function(){
-    'use strict';
-
-    /*var socket = io();
-      socket.on('connection', function() {
-        console.log('Connected!');
-      });
-      
-      socket.on('tweets', function(tweet, score) {
-
-        if(score > 0) {
-            var html = '<p class="bg-success">' + tweet + '</p>';
-        } else {
-             var html = '<p class="bg-danger">' + tweet + '</p>';
-        }
-        $('#tweet-container').append(html);
-      });*/
-
-        new app.ItemView();
+    'use strict'; 
+    
+    new app.ItemView();
 });
